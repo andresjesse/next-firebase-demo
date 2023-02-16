e19280d1ff69918083fa90c851ab842ad24b1c8a
Andres <andres@pop-os.localdomain>
2023-02-16 15:43:03 -0300

    configured ui lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import firebaseConfig from "@/config/firebaseConfig";
+import { Center, useMantineTheme } from "@mantine/core";
 import { FirebaseAppProvider } from "reactfire";
 import "./globals.css";
+import RootStyleRegistry from "./RootStyleRegistry";
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const theme = useMantineTheme();
+
   return (
     <html lang="en">
       {/*
@@ -18,7 +22,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body>
         <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-          {children}
+          <RootStyleRegistry>
+            <Center mih="100vh" bg={theme.colors.gray[2]}>
+              {children}
+            </Center>
+          </RootStyleRegistry>
         </FirebaseAppProvider>
       </body>
     </html>
